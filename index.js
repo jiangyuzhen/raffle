4add667525af3f90d2485087a054106ad1bb84ac
bastarder <85257684@qq.com>
2017-01-18 14:22:33 +0800

    fork by bastarder

--- index.js
+++ index.js
@@ -1,20 +1,7 @@
-
-// var padLeft = function(val, sed='0000'){
-//   return sed.substring(0, sed.length - val.length) + val
-// }
-//
-// for (var i = 1; i <= 72; i++) {
-//   var img = document.createElement('img');
-//   img.className = `person_${padLeft(i.toString())}`;
-//   img.style.display = 'none';
-//   document.body.appendChild(img);
-//   window.setTimeout(function(){
-//     document.body.removeChild(img);
-//   },0);
-// }
-
-angular.module('mpjApp', []).controller('MpjController', function ($scope, $http, $interval, $timeout) {
-
+angular.module('mpjApp', []).controller('MpjController', function ($sce,$scope, $interval, $timeout) {
+  
+  var vm = this;
+  // init
   var prizeTypes = [
     {"type":"幸运奖", 'total':25, "everyNum":5},
     {"type":"三等奖", 'total':10, "everyNum":5},
@@ -22,110 +9,57 @@ angular.module('mpjApp', []).controller('MpjController', function ($scope, $http
     {"type":"一等奖", 'total':1, "everyNum":1}
   ]
 
-  var persions = [];
-  $scope.winner = [];
-  $scope.winnerList = [[],[],[],[]]
-  var winnerIndex = [];
-  $scope.prizeType = prizeTypes[0];
-  $scope.prizeNum = 0;
-  $scope.enterNum = 0;
-  $scope.show = false;
-  var Switch = true; //true＝可以开始　false＝停止
-  var timer;
+  var timer = null;
+  var defaultMusic = './raffle.mp3';
+  var secUrl = $sce.trustAsResourceUrl;
 
-  function init(){
-    // 初始化数据
-    persions = names.persons;
-  	// $http.get('./name.json').success(function(data) {
-  	// 	persions = data.persons;
-  	// })
+  vm.prizeTypes = prizeTypes;
+  vm.round = -1;
+  vm.win = [];
+  vm.winList = [[],[],[],[]];
+  vm.audioSrc = defaultMusic;
+  
+  document.addEventListener('keyup',keyUpEvent);
+
+  function keyUpEvent(){
+    event.keyCode === 13 && (timer ? $scope.$apply(stop) : $scope.$apply(start));
   }
 
-  var menuZone = angular.element(document);
-  var audio = document.getElementById('audio');
-  menuZone.bind("keyup", function (event) {
-    // 开始
-    if (event.keyCode == 13 &&  Switch && $scope.enterNum<4) {
-      audio.src = './raffle.mp3';
-      audio.play();
-      $scope.show = true;
-      $scope.winner = [];
-      createInterval();
-    }else if(event.keyCode == 13 &&  !Switch && $scope.enterNum<4){
-      // 暂停
-      RandomMusic();
-      closeInterval();
+  function start(){
+    vm.audioSrc = defaultMusic;
+    if(vm.round < 0 || (vm.winList[vm.round].length === vm.prizeTypes[vm.round].total)){
+      vm.round ++;
     }
+    timer = $interval(function(){
+      let i = 0;
+      let sameList = [];
+      let winner = [];
+      while(i !== vm.prizeTypes[vm.round].everyNum){
+        let index = parseInt(Math.random() * 72);
+        let p = names.persons[index];
+        if(p.type || ~sameList.indexOf(index)){
+          continue;
+        }
+        sameList.push(index);
+        winner[i++] = p;
+      }
+      vm.win = winner;
+    },100)
+  }
 
-    if($scope.enterNum==4){
-      $scope.$apply(function(){
-        $scope.enterNum = 4;
-      });
-    }
-  })
-
-  // 开启定时器
-  function createInterval() {
-		timer = $interval(function () {
-			$scope.winner = Random($scope.prizeType.everyNum);
-		}, 100);
-    Switch = false;
-	}
-  // 关闭定时器
-  function closeInterval() {
+  function stop(){
     $interval.cancel(timer);
-    $scope.$apply(function(){
-      $scope.prizeNum　= $scope.prizeNum + $scope.prizeType.everyNum;
-      $scope.winner = Random($scope.prizeType.everyNum);
-    });
-    devareWinner();
-    Switch = true;
-    if($scope.prizeType.total == $scope.prizeNum){
-      $scope.enterNum ++;
-      $scope.prizeType = prizeTypes[$scope.enterNum];
-      $scope.prizeNum = 0;
+    timer = null;
+    vm.win.each(function(p){
+      p.type = prizeTypes[vm.round].type;
+    })
+    vm.winList[vm.round] = vm.winList[vm.round].concat(vm.win);
+    if(vm.round === prizeTypes.length-1){
+      vm.round = 4;
+      vm.winList.reverse();
+      document.removeEventListener('keyup',keyUpEvent);
     }
-    // alert("确定已经领奖了吗？")
-	}
-  // 滚动
-  function Random(n) {
-    var arr = [];
-    winnerIndex = [];
-    for (var i = 0; i < n; i++) {
-      var index = createRandom();
-      arr.push(persions[index]);
-    }
-    return arr;
-  }
-  function createRandom() {
-    var random = Math.random();
-    var index = parseInt(random * (persions.length));
-    if(winnerIndex.indexOf(index) != -1){
-      return createRandom();
-    }else{
-      winnerIndex.push(index);
-      return index;
-    }
-  }
-  // 取出N个人员
-  function devareWinner() {
-    if(winnerIndex.length>1){
-      winnerIndex.sort(function(a,b){
-        return b - a ;
-      })
-    }
-    winnerIndex.map(function(index,key){
-      var OO = Object.assign({},persions[winnerIndex[key]],{"type":$scope.prizeType.type})
-      $scope.winnerList[3-$scope.enterNum].push(OO);
-      persions.splice(winnerIndex[key],1)
-    });
+    vm.audioSrc = './raffle' + (parseInt(Math.random() * 4) + 1) + '.mp3'
   }
 
-  function RandomMusic(){
-    var index = parseInt(Math.random() * 4) + 1;
-    audio.src = './raffle' + index + '.mp3';
-    audio.play();
-  }
-
-  init();
 });
